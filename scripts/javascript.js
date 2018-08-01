e8e4712cdce522e086a9dbe3b4c89840a84b830d
Kenneth <rekerc4@gmail.com>
2018-07-31 21:38:52 -0400

    Almost Done

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,11 +1,29 @@
+"use strict";
+
+let toggle = null; 
+let resArr = {};
+let tableNum = 0;
+
+
+
 $(document).ready(function () {
+
+    let carryData = undefined;
+    let currentT = undefined; 
 
 function registerEventHandlers(){
     $("body").on("click", "#tables .table", (e) => {
-        console.log($(e));
         let eyeD = $(e.target).attr("id");
-        console.log(eyeD);
-        console.log($(e.target).parent().attr("id"));
+        // $("body").on("mouseenter mouseleave", "#tables .table", function(){
+        //     console.log($(carryData).data("name"));
+        // })
+        console.log(e); 
+       
+        
+        currentT = e.currentTarget.id;
+
+       
+       
         if(eyeD === undefined){
            let pcId =  $(e.target).parent().attr("id");
            let rese = checkIfToggle(pcId);
@@ -21,18 +39,44 @@ function registerEventHandlers(){
     });
 
     $("#exit").click(function(){
+       
+        $("#name").removeClass("reserve");
         $("#form").toggleClass("hidden");
-        $("#form").css("opacity", 0.0);
+        $("#form").fadeTo(2000, 0.0);
+        $("#tables").click(function(e){
+            console.log($(carryData).data("num"));
+        })
+        
     });
 
-    $("#save").click(function(){
-        $("#form").css("opacity", 0.0);
+    $("#save").click(function(e){
+        console.log(currentT); 
+        carryData = $(e.target);
+        carryData.data("name", $("#name").val()); 
+        carryData.data("num", $("#numSelect").val());
+        $("#form").fadeTo(2000, 0.0);
         $("#form").toggleClass("hidden");
-        let r = $("#table-num-label").text().replace(/\D/g,'');
-        let iden = "#table" + r;
-        $(iden).toggleClass("reserved");
-    });
+        let theTable = "#table" + tableNum;
+        $(theTable).toggleClass("reserved");
+        newMOver();
+     });
 
+    let newMOver = (target) => {
+        let cId = "#" + currentT;
+        let na = carryData.data("name");
+        let nu = carryData.data("num");
+        $(cId).mouseover(function(){
+            console.log(typeof na);
+            console.log(nu);
+        })
+
+        $(cId).mouseleave(function(){
+            console.log(na);
+            console.log(typeof nu);
+        })
+           console.log(currentT);
+    }
+  
     // $("body").on("click", "#tables .table p", (e) => {
     //     console.log($(e.target));
     //     let d = $(e.target).parent();
@@ -42,23 +86,24 @@ function registerEventHandlers(){
 
 function checkIfToggle(check){
     let theCheck = "#" + check; 
-    let res = $(theCheck).hasClass("reserved");
-    console.log(res);
+    let res = $(theCheck).hasClass("reserve");
     return res;
 }
 
 function showForm(eyeD, rese){
-    console.log(rese); 
+
 
     let theNum = parseInt(eyeD.replace(/\D/g,''));
-    console.log(theNum);
+    tableNum = theNum; 
+
     if(rese === true){
 
     }
     else{
         $("#form").toggleClass("hidden");
         $("#form").fadeTo(2000, 1.0);
-        $("#table-num-label").append(theNum);
+        $("#table-num-label").children("p").eq(0).remove();
+        $("#table-num-label").append(`<p class="inline" id="theNum">${theNum}</p>`);
     }
     
 }
